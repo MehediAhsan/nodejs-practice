b7949d1bf49a6b5adf36298ae4a4acb91f755f81
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-04-05 00:49:51 +0600

    req res handle

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const {add} = require('./add');
+// const {add} = require('./add');
 
-console.log(add(2,3));
+// console.log(add(2,3));
+
+const http = require('http');
+
+const server = http.createServer((req, res)=>{
+    if(req.url == '/'){
+        res.writeHead(200,{'content-type':'text/html'})
+        res.write('<p>Hi vai</p>')
+        res.end()
+    }
+
+    else if(req.url == '/product'){
+        res.writeHead(200,{'content-type':'application/json'})
+        res.end(JSON.stringify({id:1,name:'watch',price:20}))
+    }
+
+});
+const port = 5000;
+server.listen(port);
+
+console.log(`server running at ${port}`);
