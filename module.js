71ab08a81cc6bb77ed4cf59c2ff0eed6d58519a7
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-04-28 23:51:05 +0600

    stream add

--- module.js
+++ module.js
@@ -1,3 +1,17 @@
 const os = require('node:os'); 
 
-console.log(os.version())
+// console.log(os.cpus())
+
+const EventEmitter = require('node:events')
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+
+myEmitter.on('event', () => {
+  console.log('an event occurred!');
+});
+
+setTimeout(() => {    
+    myEmitter.emit('event');
+}, 2000);
