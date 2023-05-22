db3e45cd1f8664c53c5bd506249e2c4e2559abe3
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-05-22 21:21:17 +0600

    token created

--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 // dependencies
 const { homeHandler } = require('./handleRoutes/homeHandler');
 const {userHandler} = require('./handleRoutes/userHandler');
+const {tokenHandler} = require('./handleRoutes/tokenHandler');
 
 const routes = {
     home: homeHandler,
-    user: userHandler
+    user: userHandler,
+    token: tokenHandler
 };
 
 module.exports = routes;
